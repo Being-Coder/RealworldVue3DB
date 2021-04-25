11024cd50e3e382593ce7bb8c761f0edbd10d742
Being-Coder <aditya221127@gmail.com>
2021-04-25 14:39:30 +0530

    mixed content error

--- src/services/EventServices.js
+++ src/services/EventServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL : 'http://my-json-server.typicode.com/Being-Coder/RealworldVue3DB',
+    baseURL : 'https://my-json-server.typicode.com/Being-Coder/RealworldVue3DB',
     withCredentials : false,
     headers : {
         Accept : 'application/json',
